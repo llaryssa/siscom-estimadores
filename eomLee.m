edd0b20d4b447fb3f30f0e82031c976ff6de8ceb
Laryssa Seabra <alsas@cin.ufpe.br>
2015-05-20 11:06:18 -0300

    Create eomLee.m

---
+++ eomLee.m
@@ -0,0 +1,24 @@
+function [frames, nchapeu] = eomLee (L, ss, sc)
+    thres = 0.0015;
+    ended = 0;
+    yant = 2;
+
+    while (~ended)
+        e = exp(1); % num de euler
+        b = L / (yant * sc + ss);
+        y = (1 - e^(-1/b)) / (b * (1 - (1 + 1/b)*e^(-1/b)));      
+    
+        frames = y*sc;
+        nchapeu = frames/b;
+
+        frames = round(frames);
+        nchapeu = round(nchapeu);
+    
+        if abs(yant-y) < thres 
+            ended = 1;
+        else
+            ended = 0;
+        end
+        yant = y;
+    end    
+end
